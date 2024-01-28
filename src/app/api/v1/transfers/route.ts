54143ecdb8981436a519527c8432902ac708ce4e
Pabloricheli <Pabloricheli@live.com>
2024-01-27 23:08:46 -0300

    tratamentos erros transfer

--- src/app/api/v1/transfers/route.ts
+++ src/app/api/v1/transfers/route.ts
@@ -6,21 +6,30 @@ import {type NextRequest, NextResponse } from "next/server"
 export async function POST(req: NextRequest) {
   const data = await req.json()
   try { 
+   
+      await api.post('transfers', data)
+        .then(async response => {         
+           
+          await db.collection('users').doc(data.userId).collection('transfers').add(response.data());
+
+        })
+        .catch((err) => {
+          const error = err as any
+          return NextResponse.json(error.response.data.errors[0].description, {
+            status: error.response.status
+          })
+        })
+        
+        await db.collection('indications').doc(data.indicationId).update({
+          redeem: true,
+          redeemAt: new Date()
+        })
   
-    const response = await api.post('transfers', data)
+ 
 
-    db.collection('indications').doc(data.indicationId).update({
-      redeem: true,
-      redeemAt: new Date()
-    })
-
-    db.collection('users').doc(data.userId).collection('transfers').add(response.data());
-
-    return NextResponse.json(response.data() , {  status: 201 })
+    return NextResponse.json('Tranferência pedida com sucesso' , {  status: 201 })
   } catch (err) {
     const error = err as any
-    return NextResponse.json(error.response.data.errors[0].description, {
-      status: error.response.status
-    })
+    return NextResponse.json(error.message, {status: 500})
   }
 }
