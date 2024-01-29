59f208f541de895e85f6e97a244da31ba9d78e98
Francisco Freitas Jr <fresfj@gmail.com>
2024-01-28 21:25:34 -0300

    atualização webhook e transfer

--- src/app/api/v1/transfers/route.ts
+++ src/app/api/v1/transfers/route.ts
@@ -1,11 +1,14 @@
-import api from "@/app/utils/api"
-import { db } from "@/app/utils/data/firebase-admin-config";
+import api from '@/app/utils/api'
+import { db } from '@/app/utils/data/firebase-admin-config'
+import { type NextRequest, NextResponse } from 'next/server'
 
-import {type NextRequest, NextResponse } from "next/server"
+export async function GET(req: NextRequest) {
+  return NextResponse.json('method unavailable', { status: 200 })
+}
 
 export async function POST(req: NextRequest) {
-  try { 
-    const {    
+  try {
+    const {
       bankAccount,
       description,
       operationType,
@@ -14,7 +17,7 @@ export async function POST(req: NextRequest) {
       scheduleDate,
       value,
       indicationId,
-      userId    
+      userId
     } = await req.json()
 
     const dataSend = {
@@ -22,35 +25,38 @@ export async function POST(req: NextRequest) {
       operationType,
       scheduleDate,
       value,
-      ...(operationType === 'PIX' ? {pixAddressKey, pixAddressKeyType} : {bankAccount})    }
-    
-    
-    const response = await api.post('transfers', dataSend) 
- 
-              
-    const transfer = await db.collection('users').doc(userId).collection('transfers').add(response.data);
+      ...(operationType === 'PIX'
+        ? { pixAddressKey, pixAddressKeyType }
+        : { bankAccount })
+    }
 
-  
+    const response = await api.post('transfers', dataSend)
+
+    const transfer = await db
+      .collection('users')
+      .doc(userId)
+      .collection('transfers')
+      .add(response.data)
+
     await db.collection('indications').doc(indicationId).update({
+      tid: response.data.id,
       redeemId: transfer.id,
       redeem: true,
       redeemAt: new Date()
     })
 
-    
-    return NextResponse.json('Tranferência pedida com sucesso' , {  status: 201 })
+    return NextResponse.json('Tranferência pedida com sucesso', { status: 201 })
   } catch (err) {
     const error = err as any
-   
-    if(error.response.data.errors) {
+
+    if (error.response.data.errors) {
       return NextResponse.json(error.response.data.errors[0].description, {
         status: error.response.status
       })
     }
 
     return NextResponse.json(error.message || 'Erro durante a transferência.', {
-      status: error.status || 500,
-    });
-    
+      status: error.status || 500
+    })
   }
 }
--- src/app/api/v1/webhook/route.ts
+++ src/app/api/v1/webhook/route.ts
@@ -1,27 +1,51 @@
+import { type NextRequest, NextResponse } from 'next/server'
+import { db } from '@/app/utils/data/firebase-admin-config'
 
-import {type NextRequest, NextResponse } from "next/server"
-import { db } from "@/app/utils/data/firebase-admin-config"
+export async function GET(req: NextRequest) {
+  return NextResponse.json('method unavailable', { status: 200 })
+}
 
 export async function POST(req: NextRequest) {
   try {
-    const data = await req.json();
+    const data = await req.json()
 
-    if( data.event === "TRANSFER_CREATED") {
-      //  atualizar tabela de transfer usuario
-    }
+    if (data.event === 'TRANSFER_DONE') {
+      const response = await db
+        .collection('indications')
+        .where('tid', '==', data.transfer.id)
+        .get()
 
-    if (data.event === "PAYMENT_RECEIVED" && data.payment?.billingType === 'PIX') {     
-      const indicationsQuery = await db.collection('indications').where('payment.id', '==', data.payment.id).get();
-      
-      indicationsQuery.docs.map(async (indicationDoc) => {
+      response.docs.map(async indicationDoc => {
+        await db
+          .collection('users')
+          .doc(indicationDoc.data().idaluno)
+          .collection('transfers')
+          .doc(indicationDoc.data().tid)
+          .set(data.transfer)
+
+        await db
+          .collection('indications')
+          .doc(indicationDoc.id)
+          .update({ pay: true })
+      })
+    } else if (
+      data.event === 'PAYMENT_RECEIVED' &&
+      data.payment?.billingType === 'PIX'
+    ) {
+      const indicationsQuery = await db
+        .collection('indications')
+        .where('payment.id', '==', data.payment.id)
+        .get()
+
+      indicationsQuery.docs.map(async indicationDoc => {
         await db.collection('indications').doc(indicationDoc.id).update({
           payment: data.payment,
           status: true
         })
-      });
+      })
     }
 
-    return NextResponse.json('Efetuado com sucesso', { status: 201 })
+    return NextResponse.json('Efetuado com sucesso', { status: 200 })
   } catch (err) {
     const error = err as Error
     return NextResponse.json(error.message, { status: 500 })
